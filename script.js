7aa9a50ab0bd3956624fa14c1e87472e61ccb754
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-26 21:28:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
-function firstChar(text) {
+function firstChar(str) {
   // your code here
+	 // Loop through each character in the string
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== ' ') {
+      return str[i];  // Return the first non-space character
+    }
+  }
+  return '';  // Return empty string if no non-space character is found
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-//const text = prompt("Enter text:");
+const text = prompt("Enter text:");
 alert(firstChar(text));
